40178bf2a54744d8756822b9168aaf2284f73ddc
tasthana <tasthana@unionmutual.com>
2024-09-25 15:50:45 -0400

    insta feed dupe done 1/2

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('/writes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/uploads/writes.html'));
 });
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000...');
+app.listen(3001, () => {
+    console.log('Server is listening on port 3001...');
 });
